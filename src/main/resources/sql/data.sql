a6687abaf92f99c07fbaf8d6f370af7a1fdc3024
nicekkong <nicekkong@gmail.com>
2022-06-14 01:33:11 +0900

    Entity 연관관계 수정

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,20 @@
+insert into tb_employee (id, name, dept_name)
+values (1, 'nicekkong@gmail.com', 'CEO');
+
+insert into tb_employee (id, name, dept_name)
+values (2, 'nicekkong', 'CFO');
+
+insert into tb_employee (id, name, dept_name)
+values (3, 'Kong', 'Planning Team');
+
+insert into tb_employee (id, name, dept_name)
+values (4, 'Lee');
+
+insert into tb_employee (id, name, dept_name)
+values (5, 'Choi', 'IT Team');
+
+insert into tb_employee (id, name, dept_name)
+values (6, 'Park', 'IT Team');
+
+insert into tb_employee (id, name, dept_name)
+values (7, 'Johnson', null);
